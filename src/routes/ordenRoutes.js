1f79219448904dfeefd1303dfec10f5e7287c192
Jesus Yael Padrón Grimaldo <jesusyaelpadrongrimaldo@gmail.com>
2025-10-21 21:03:50 -0600

    Se suben cambios para que funcione todo

--- src/routes/ordenRoutes.js
+++ src/routes/ordenRoutes.js
@@ -6,7 +6,7 @@ import {
   actualizarOrden,
   eliminarOrden,
 } from "../controllers/ordenController.js";
-<<<<<<< HEAD
+
 import { proteger } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
@@ -16,15 +16,6 @@ router.get("/", proteger, obtenerOrdenes);
 router.get("/:id", proteger, obtenerOrdenPorId);
 router.put("/:id", proteger, actualizarOrden);
 router.delete("/:id", proteger, eliminarOrden);
-=======
 
-const router = express.Router();
-
-router.post("/", crearOrden);
-router.get("/", obtenerOrdenes);
-router.get("/:id", obtenerOrdenPorId);
-router.put("/:id", actualizarOrden);
-router.delete("/:id", eliminarOrden);
->>>>>>> c3f965d4d770e2488edd6a975c5a34ea863b974d
 
 export default router;
--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -6,27 +6,16 @@ import {
   actualizarProducto,
   eliminarProducto,
 } from "../controllers/productoController.js";
-<<<<<<< HEAD
+
 import { proteger, esAdmin } from "../middleware/authMiddleware.js";
-=======
-import { esAdmin } from "../middleware/authMiddleware.js";
->>>>>>> c3f965d4d770e2488edd6a975c5a34ea863b974d
 
 const router = express.Router();
 
 // Rutas CRUD
-<<<<<<< HEAD
 router.post("/", proteger, esAdmin,crearProducto); // Crear
 router.get("/", proteger, obtenerProductos); // Leer todos
 router.get("/:id", proteger, obtenerProductoPorId); // Leer uno
 router.put("/:id", proteger, esAdmin,actualizarProducto); // Actualizar
 router.delete("/:id", proteger, esAdmin,eliminarProducto); // Eliminar
-=======
-router.post("/", esAdmin,crearProducto); // Crear
-router.get("/", obtenerProductos); // Leer todos
-router.get("/:id", obtenerProductoPorId); // Leer uno
-router.put("/:id", esAdmin,actualizarProducto); // Actualizar
-router.delete("/:id", esAdmin,eliminarProducto); // Eliminar
->>>>>>> c3f965d4d770e2488edd6a975c5a34ea863b974d
 
 export default router;
